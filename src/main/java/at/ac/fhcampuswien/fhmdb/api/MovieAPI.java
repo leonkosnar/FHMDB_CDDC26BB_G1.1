d31ef73a1d03a09bf6bc89e1d91e7f5246a4fe57
David Lisnjic <david.lisnjic@stud.fh-campuswien.ac.at>
2024-04-10 19:22:56 +0200

    Adjusted pom.xml (added gson) and created a MovieApi class with the api logic. Added gson to module module-info.java

--- src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
@@ -1,8 +1,67 @@
 package at.ac.fhcampuswien.fhmdb.api;
 
 import at.ac.fhcampuswien.fhmdb.models.Movie;
+import com.google.gson.Gson;
 import okhttp3.*;
 
+import java.io.IOException;
+import java.util.List;
+
+
 public class MovieAPI {
-    // TODO implement
+    private static final String BASE_URL = "https://prog2.fh-campuswien.ac.at";
+    private static final String MOVIES_ENDPOINT = "/movies";
+
+    private final OkHttpClient httpClient;
+    private final Gson gson;
+
+    public MovieAPI() {
+        this.httpClient = new OkHttpClient();
+        this.gson = new Gson();
+    }
+
+    public List<Movie> getAllMovies() throws IOException {
+        Request request = new Request.Builder()
+                .url(BASE_URL + MOVIES_ENDPOINT)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            ResponseBody body = response.body();
+            if (body != null) {
+                String json = body.string();
+                return gson.fromJson(json, List.class);
+            }
+        }
+
+        return null;
+    }
+
+    public List<Movie> searchMovies(String query, String genre) throws IOException {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL + MOVIES_ENDPOINT).newBuilder();
+        urlBuilder.addQueryParameter("query", query);
+        urlBuilder.addQueryParameter("genre", genre);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            ResponseBody body = response.body();
+            if (body != null) {
+                String json = body.string();
+                return gson.fromJson(json, List.class);
+            }
+        }
+
+        return null;
+    }
 }
