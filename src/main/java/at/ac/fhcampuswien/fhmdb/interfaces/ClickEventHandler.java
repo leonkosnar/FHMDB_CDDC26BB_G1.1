7cbba22af44171e08d84bdbeab54424751226326
HaciGustav <mehmetfkuruldak@gmail.com>
2024-05-15 22:12:51 +0200

    Add ClickEventHandler, home-view, Genre. Add switch between scenes feature...

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/interfaces/ClickEventHandler.java
@@ -0,0 +1,6 @@
+package at.ac.fhcampuswien.fhmdb.interfaces;
+
+@FunctionalInterface
+public interface ClickEventHandler<T> {
+    void onClick(T t);
+}
--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Genre.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Genre.java
@@ -1,4 +1,4 @@
-package at.ac.fhcampuswien.fhmdb;
+package at.ac.fhcampuswien.fhmdb.models;
 
 public enum Genre {
     ACTION,
